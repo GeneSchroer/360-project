65d93c92fdf01eecfadbf299fabcb36a95b4593e
Gene Schroer <gene.schroer@stonybrook.edu>
2017-05-04 16:38:30 -0700

    add include folder

---
+++ include/ids.h
@@ -0,0 +1,15 @@
+#IFNDEF IDSHEADER
+
+#DEFINE IDSHEADER
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ptrace.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/reg.h>
+
+#ENDIF
