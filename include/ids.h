973e51178ebb4fdb29e0991f5534342eb0e96bbd
Gene Schroer <gene.schroer@stonybrook.edu>
2017-05-10 11:16:34 -0700

    tweak ids.h so that the bucket holds an array of ngram and not ngram pointers, and that the profile holds an array of buckets and not bucket pointers. Also, add functions to util.c so that you can see if an ngram is in a bucket

--- include/ids.h
+++ include/ids.h
@@ -11,12 +11,13 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/reg.h>
-
+#include <sys/syscall.h>
 #define NGRAM_SIZE 3
+#define HASH 4
 
 // Holds all information for a program's IDS profile
 typedef struct{
-	// A lis of all directories and files accessible by this program
+	// A list of all directories and files accessible by this program
 	char** directories;
 	int numDirectories;
 	
@@ -25,14 +26,14 @@ typedef struct{
 	
 	// List of 4 ngram buckets in which ngrams will be placed. The bucket number is determined by taking ngram->sysCalls[0] % 4
 	// This simple hash will reduce search times for an existing ngram sequence in both training mode and defense mode
-	ngramBucket** ngramBuckets;
+	ngramBucket* ngramBuckets;
 	int numNgramBuckets;
 }Profile;
 
 // Holds a list of ngrams. Bucket number is determined using the algorithm mentioned above
 typedef struct{
 	int numNgrams;
-	ngram** ngrams;
+	ngram* ngrams;
 }ngramBucket;
 
 // A list of at most three system calls recorded from the program
