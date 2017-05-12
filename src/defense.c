e143e1434889183dc83ebb5d1e7c2977261385ea
Gene Schroer <gene.schroer@stonybrook.edu>
2017-05-12 14:16:23 -0700

    modify defense.c to store an ngram and compare it to the ngrams in the profile. Also, add a function to util.c to check if an ngram is in a profile

--- src/defense.c
+++ src/defense.c
@@ -6,7 +6,18 @@ int run_defense_mode(char *pathname, char** new_argv){
   int status;
   int insyscall = 0;
   ngram n;
-  n->syscalls = malloc(sizeof(int) * (NGRAM_SIZE + 1));
+  Profile *profile = malloc(sizeof(Profile));
+  int i;
+  /* Step 1: Load a profile */
+  char* programName = getProgramName(pathname);
+  profile = (Profile*)loadProfile(programnNme);
+
+  
+  n->sysCalls = malloc(sizeof(int) * (NGRAM_SIZE + 1));
+
+
+
+  /* Step ? - Create a child process and execute the input program */
   child = fork();
   if(child==0){
     ptrace(PTRACE_TRACEME, 0, NULL, NULL);
@@ -14,27 +25,37 @@ int run_defense_mode(char *pathname, char** new_argv){
   }
   else{
     while(1){
-      wait(&status);
-      if(WIFEXITED(status))
-	break;
+      for(i = 0; i<NGRAM_SIZE; ++i){
+	
+	wait(&status);
+	if(WIFEXITED(status))
+	  break;
 	orig_eax = ptrace(PTRACE_PEEKUSER, child, 4 * ORIG_EAX, NULL);
 	//	if(orig_eax == SYS_write){
-	  //  if(insyscall == 0){
-	  //insyscall = 1;
-	    ebx = ptrace(PTRACE_PEEKUSER, child, 4 * EBX, NULL);
-	    edx = ptrace(PTRACE_PEEKUSER, child, 4 * EDX, NULL);
-
-	    //	ebx = ptrace(PTRACE_PEEKUSER, child, 4 * EBX, NULL);
-	    printf("Write called with: %ld %ld %ld\n", orig_eax, ebx, edx);
-	    //	  }
+	//  if(insyscall == 0){
+	//insyscall = 1;
+	n->sysCalls[i] = (int) orig_eax;
+	ebx = ptrace(PTRACE_PEEKUSER, child, 4 * EBX, NULL);
+	edx = ptrace(PTRACE_PEEKUSER, child, 4 * EDX, NULL);
+	
+	//	ebx = ptrace(PTRACE_PEEKUSER, child, 4 * EBX, NULL);
+	printf("Write called with: %ld %ld %ld\n", orig_eax, ebx, edx);
+	//	  }
 	//else{
 	//  eax = ptrace(PTRACE_PEEKUSER, child, 4 * EAX, NULL);
 	//  printf("Write returned "
 	//		   "with %ld\n", eax);
-	    //insyscall = 0;
-    //}
+	//insyscall = 0;
 	//}
+	//}
+      
 	ptrace(PTRACE_SYSCALL, child, NULL, NULL);
+      }
+      /* if the ngram is not part of the profile, kill the program */
+      if(isValidNgram(n, profile) == 0){
+	ptrace(PTRACE_KILL, child, NULL, NULL);
+      }
+      
     }
   }
   return 0;
